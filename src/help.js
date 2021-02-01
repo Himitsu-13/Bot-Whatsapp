f7e9a1246dbff8192215bad81a814955942c617a
Mr-Paradox13 <77006289+Mr-Paradox13@users.noreply.github.com>
2021-02-01 07:45:49 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,182 @@
+const help = (prefix) => { 
+	return `
+	*🍁MENU ParadoX - Bot 🍁*
+Version : 2.0
+⊱✦•* 𝑓𝑖𝑡𝑢𝑟
+▋╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅   
+▋┋*${prefix}owner
+▋┋*${prefix}donasi
+▋┋*${prefix}repot
+▋┋*${prefix}speed
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+╭────────────────────╮
+*➼✰︙   𝑀𝑒𝑑𝑖𝑎
+╰────────────────────╯
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+▋┋*${prefix}sticker
+▋┋*${prefix}tsticker
+▋┋*${prefix}toimg
+▋┋*${prefix}wait
+▋┋*${prefix}kbbi
+▋┋*${prefix}imoji
+▋┋*${prefix}url2img
+▋┋*${prefix}playstore
+▋┋*${prefix}babi
+▋┋*${prefix}anjing
+▋┋*${prefix}unta
+▋┋*${prefix}elag
+▋┋*${prefix}inu
+▋┋*${prefix}joox
+▋┋*${prefix}randomcat
+▋┋*${prefix}wallpaperhd
+▋┋*${prefix}ssweb
+▋┋*${prefix}memeindo
+▋┋*${prefix}ttp
+▋┋*${prefix}meme
+▋┋*${prefix}tep
+▋┋*${prefix}tts
+▋┋*${prefix}tes
+▋┋*${prefix}pinterest
+▋┋*${prefix}image
+▋┋*${prefix}igstalk
+▋┋*${prefix}fototiktok
+▋┋*${prefix}tiktokstalk
+▋┋*${prefix}tiktok
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+╭────────────────────╮
+*➼︙   𝐴𝑛𝑖𝑚𝑒 
+╰────────────────────╯
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+▋┋*${prefix}anime Error
+▋┋*${prefix}loli Error
+▋┋*${prefix}loli2 Error
+▋┋*${prefix}waifu Error
+▋┋*${prefix}waifu2 error
+▋┋*${prefix}wibu Error
+▋┋*${prefix}randomanime Error
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+╭────────────────────╮
+*➼✰︙    𝐹𝑢𝑛
+╰────────────────────╯
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+▋┋*${prefix}artinama
+▋┋*${prefix}truth
+▋┋*${prefix}dare
+▋┋*${prefix}tebakgambar
+▋┋*${prefix}family100
+▋┋*${prefix}caklontong
+▋┋*${prefix}game
+▋┋*${prefix}primbonjodoh
+▋┋*${prefix}ramaljodoh
+▋┋*${prefix}ramaljadian
+▋┋*${prefix}mlherolist
+▋┋*${prefix}bucin
+▋┋*${prefix}persengay
+▋┋*${prefix}ramalhp
+▋┋*${prefix}cekjodoh
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+╭────────────────────╮
+*➼✰︙    𝐼𝑛𝑓𝑜
+╰────────────────────╯
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+▋┋*${prefix}infogc
+▋┋*${prefix}infogempa
+▋┋*${prefix}infocuaca
+▋┋*${prefix}infonomor
+▋┋*${prefix}infomobil
+▋┋*${prefix}infomotor
+▋┋*${prefix}groupinfo
+▋┋*${prefix}lirik
+▋┋*${prefix}quotes
+▋┋*${prefix}cerpen
+▋┋*${prefix}chord
+▋┋*${prefix}wiki
+▋┋*${prefix}brainly
+▋┋*${prefix}resepmakanan
+▋┋*${prefix}map
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+╭────────────────────╮
+*➼✰     𝐺𝑟𝑜𝑢𝑝
+╰────────────────────╯
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+▋┋*${prefix}add
+▋┋*${prefix}kick
+▋┋*${prefix}promote
+▋┋*${prefix}demote
+▋┋*${prefix}setname
+▋┋*${prefix}setdesc
+▋┋*${prefix}welcome
+▋┋*${prefix}nsfw
+▋┋*${prefix}simih
+▋┋*${prefix}group
+▋┋*${prefix}tagme
+▋┋*${prefix}hidetag
+▋┋*${prefix}tag
+▋┋*${prefix}tagall
+▋┋*${prefix}fitnah
+▋┋*${prefix}infogc
+▋┋*${prefix}groupinfo
+▋┋*${prefix}linkgroup
+▋┋*${prefix}listadmins
+▋┋*${prefix}openanime
+▋┋*${prefix}edotense
+▋┋*${prefix}kudeta
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+╭────────────────────╮
+*➼✰︙𝑁𝑠𝑓𝑤 𝑀𝑒𝑛𝑢
+╰────────────────────╯
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+▋┋*${prefix}nsfwloli
+▋┋*${prefix}nswflowjob
+▋┋*${prefix}nsfwneko
+▋┋*${prefix}nsfwtrap
+▋┋*${prefix}randomhentai
+▋┋*${prefix}hentai
+▋┋*${prefix}indohot
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+╭────────────────────╮
+*➼✰︙ 𝐾𝑒𝑟𝑎𝑛𝑔
+╰────────────────────╯
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+▋┋*${prefix}apakah
+▋┋*${prefix}kapankah
+▋┋*${prefix}bisakah
+▋┋*${prefix}rate
+▋┋*${prefix}watak
+▋┋*${prefix}hobi
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+╭────────────────────╮
+*➼✰  ︙𝑂𝑡ℎ𝑒𝑟𝑠 
+╰────────────────────╯
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+▋┋*${prefix}blosklist
+▋┋*${prefix}say
+▋┋*${prefix}hilih
+▋┋*${prefix}testime
+▋┋*${prefix}delete
+▋┋*${prefix}shorturl
+▋┋*${prefix}spam (30 pesan)
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+╭────────────────────╮
+*➼✰︙  𝑂𝑤𝑛𝑒𝑟
+╰────────────────────╯
+■█■█■█■▰▱▰▱▰▱■█■█■█■
+▋┋*${prefix}bc -promosi*
+▋┋*${prefix}ban -banned
+▋┋*${prefix}block -blok
+▋┋*${prefix}unblok
+▋┋*${prefix}clearall
+▋┋*${prefix}clone
+▋┋*${prefix}getses
+▋┋*${prefix}setpp
+▋┋*${prefix}leave
+═️ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ۣٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜۜ͜͡♕͜͡═️ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ۣٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜۜ͜͡♕͜͡═️ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ۣٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜۜ͜͡♕͜͡═️ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ۣٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜۜ͜͡♕͜͡═️ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ۣٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜۜ͜͡♕͜͡═️ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ۣٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜۜ͜͡♕͜͡═️ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ۣٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜۜ͜͡♕͜͡═️ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ۣٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜۜ͜͡♕͜͡═️ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ۣٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜۜ͜͡♕͜͡═️ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ۣٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜۜ͜͡♕͜͡
+*(•♛•)─•••──ೇุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุุ──────﹒ׂׂૢ་༘࿐ೢִֶָ──╮*
+*▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄*        
+    *┏ೋ┉━┉ೋ✧ೋ┉━┉ೋ┓*
+        Gunakan Dengan Bijak
+    *┗ೋ┉━┉ೋ✧ೋ┉━┉ೋ┛*
+*▀▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▀*       
+   ۝ٜٜٜٜٜٜٜ҈⸙ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ٜٜٜٜ✞ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ͜҈͡۝ٜٜٜٜٜٜٜ҈⸙ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ٜٜٜٜ✞ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ͜҈͡۝ٜٜٜٜٜٜٜ҈⸙ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ٜٜٜٜ✞ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ͜҈͡۝ٜٜٜٜٜٜٜ҈⸙ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ٜٜٜٜ✞ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ͜҈͡۝ٜٜٜٜٜٜٜ҈⸙ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ٜٜٜٜ✞ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ͜҈͡۝ٜٜٜٜٜٜٜ҈ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ͜҈ٜٜٜٜٜٜٜ͡҈⸙ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ҈ٜٜٜٜ✞ٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜ©`
+}
+exports.help = help
