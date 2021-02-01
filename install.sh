f74bc935c303f7bfa762618c1a7a320e0a72be39
Mr-Paradox13 <77006289+Mr-Paradox13@users.noreply.github.com>
2021-02-01 07:39:27 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+pkg update
+pkg upgrade
+pkg install nodejs
+pkg install libwebp
+pkg install ffmpeg
+pkg install wget
+pkg install tesseract
+npm install
+
+echo "[*] wes beres kabeh cok, Garek mlaku no perintah -> \"npm start\" Ben iso nyalak bot e :)"
